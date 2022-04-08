4a08a3fd45cbeda95b277ebaee39a5bec98e6212
Oleksandr Chabaniuk <olexander11441144@gmail.com>
2022-04-08 23:07:40 +0300

    pagination news feed to 10 posts in page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import axios from "axios";
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PostService from "./API/PostService";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/inpit/MyInput";
 import Loader from "./components/UI/Loader/Loader";
 import MyModal from "./components/UI/MyModal/MyModal";
-import MySelect from "./components/UI/select/MySelect";
 import { useFetching } from "./hooks/useFetching";
 import { usePosts } from "./hooks/usePost";
 import './styles/App.css';
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter,setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  let pagesArray = getPagesArray(totalPages);
+
   const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts)
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit));
   });
 
   useEffect(() => {
@@ -36,6 +39,11 @@ function App() {
   }
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const changePage = (page) => {
+    setPage(page);
+    fetchPosts();
   }
 
   return (
@@ -58,6 +66,17 @@ function App() {
         ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Пости про JS"/>
       }
+      <div className="page__wrapper">
+        {pagesArray.map(p =>
+          <span 
+            onClick={() => changePage(p)}
+            key={p} 
+            className={page === p ? 'page page__current' : 'page'}
+          >
+            {p}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
